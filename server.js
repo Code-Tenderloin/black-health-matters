5e243cd9672aefbfcbc086966936136261a2ef10
Glitch (hello-express) <none>
2020-08-09 18:54:33 +0000

    🎋😂 Checkpoint ./package.json:219809/2661 ./server.js:219809/823 ./views/layouts/main.html:219809/137 ./views/home.handlebars:219809/41 ./views/layouts/main.handlebars:219809/13

--- server.js
+++ server.js
@@ -4,29 +4,18 @@
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
+var exphbs  = require('express-handlebars');
+
 const app = express();
-
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
-
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
-
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('home', {
+      title: "Home"
+    });
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
-});
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
