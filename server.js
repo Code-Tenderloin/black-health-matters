1d0808e985acbfe498cc28b5aea6b35922c154cc
Glitch (black-health-staging) <none>
2020-10-16 23:59:07 +0000

    fix-social

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
       title: "home",
       displayTitle: 'Home',
       slug: 'Over the course of the academic year, The Spokesman will report on the disparities in healthcare for the Black community from Maryland’s largest HBCU. The series will explore medical racism, the contentious relationship between the Black community and hospitals, how COVID-19 has exacerbated Black health issues, and the barriers between Black students and medical school.',
-      thumbnailUrl: 'https://cdn.glitch.com/03f006d0-e079-4e82-9710-e203769a31d8%2Fad-2.jpg?v=1602889760649'
+      thumbnailUrl: 'https://cdn.glitch.com/03f006d0-e079-4e82-9710-e203769a31d8%2Fad-2.jpggit statu'
     });
 });
 
